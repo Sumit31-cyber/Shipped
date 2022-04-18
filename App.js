f3794e88357cfcf8a76f94f28a08a85717d1d85c
Sumit31-cyber <sumitkum9305@gmail.com>
2022-04-18 19:49:57 +0530

    Register Screen Added

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import OnBoarding from './src/screens/OnBoardingScreen';
+import RegisterScreen from './src/screens/register';
 import SignInScreen from './src/screens/signIn';
 
 export default function App() {
   return (
-    <SignInScreen />
+    // <SignInScreen />
+    // <OnBoarding />
+    <RegisterScreen />
   );
 }
 
--- src/screens/register/index.jsx
+++ src/screens/register/index.jsx
@@ -1,14 +1,102 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { styles } from "./styles.";
+import { FontAwesome5 } from "@expo/vector-icons";
+import {
+  Poppins_100Thin,
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 const RegisterScreen = () => {
+  const [visible, setVisible] = useState(true);
+
+  let [loadFonts] = useFonts({
+    Poppins_100Thin,
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!loadFonts) {
+    return <AppLoading />;
+  }
   return (
-    <View>
-      <Text>SignUpScreen</Text>
+    <View style={styles.container}>
+      <Text style={[styles.headerText, { fontFamily: "Poppins_600SemiBold" }]}>
+        Hi, Welcome
+      </Text>
+      <Text style={[styles.subTitle, { fontFamily: "Poppins_400Regular" }]}>
+        Please Register Yourself
+      </Text>
+      <TextInput style={styles.textInput} placeholder="User Name" />
+      <TextInput style={styles.textInput} placeholder="Email Address" />
+
+      <TextInput
+        secureTextEntry={visible}
+        style={styles.textInput}
+        placeholder="Password"
+      />
+      <Pressable onPress={() => setVisible(!visible)} style={styles.icon}>
+        <FontAwesome5 name="eye-slash" size={20} color="#E6E9F1" />
+      </Pressable>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Sign up</Text>
+      </Pressable>
+      <View
+        style={{
+          justifyContent: "space-evenly",
+          marginTop: 55,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <View style={styles.border}></View>
+        <Text style={styles.smallText}>or continue with</Text>
+        <View style={styles.border}></View>
+      </View>
+      <View style={styles.iconsContainer}>
+        <Pressable>
+          <Image
+            style={styles.icons}
+            source={require("../../../assets/logo/google.png")}
+          />
+        </Pressable>
+        <Pressable>
+          <Image
+            style={styles.icons}
+            source={require("../../../assets/logo/apple.png")}
+          />
+        </Pressable>
+        <Pressable>
+          <Image
+            style={styles.icons}
+            source={require("../../../assets/logo/facebook.png")}
+          />
+        </Pressable>
+      </View>
+      <Text style={[styles.recoveryPassword, { textAlign: "center" }]}>
+        Already registered? <Text style={{ color: "#5EAAA8" }}>SignIn</Text>
+      </Text>
     </View>
   );
 };
 
 export default RegisterScreen;
-
-const styles = StyleSheet.create({});
