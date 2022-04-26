35b6950841668780bd60e952c06aa93b33b459e6
Sumit31-cyber <sumitkum9305@gmail.com>
2022-04-26 21:11:52 +0530

    Lottie Animation added

--- src/screens/LoadingPage.jsx
+++ src/screens/LoadingPage.jsx
@@ -2,6 +2,7 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 import { authentication } from "../../firebaseConfig";
 import { useNavigation } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
 
 const LoadingPage = () => {
   const navigation = useNavigation();
@@ -27,6 +28,12 @@ const LoadingPage = () => {
       <Image
         style={{ height: 200, width: 200 }}
         source={require("../../assets/images/logo2.jpg")}
+      />
+      <LottieView
+        source={require("../../assets/loading.json")}
+        autoPlay
+        loop
+        style={{ marginTop: 100 }}
       />
     </View>
   );
